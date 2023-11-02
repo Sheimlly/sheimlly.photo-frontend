a6821752ecbf04d902aee31bec0509c94d48f1b2
Emilia <sheimlly.dev@gmail.com>
2023-11-02 20:02:24 +0100

    margin fix in footer

--- src/pages/partials/footer.tsx
+++ src/pages/partials/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="d-flex flex-column align-items-center">
-                <p className='my-3'><a href='/privacy_policy'>Privacy policy</a></p>
+                <p><a href='/privacy_policy'>Privacy policy</a></p>
                 <p>©2023 by Emilia  Lorentsen</p>
             </div>
         </footer>
