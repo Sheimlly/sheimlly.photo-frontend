ad46d920a5c9e9ef5670f1468ea9023b5484037f
Emilia <sheimlly.dev@gmail.com>
2023-11-02 19:53:51 +0100

    initial look of homepage and edited header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import { useState, useEffect, useRef, useLayoutEffect, createRef } from 'react';
+import axios from 'axios';
+
+interface MainPagePhotos {
+  id: number;
+  name: string;
+  session_name?: string;
+  session_name_pl?: string;
+  image: string;
+  category_name: string;
+  category_name_pl: string;
+  category: number;
+  date_created: string;
+  date_uploaded: string;
+  main_page: string;
+}
 
 function HomePage() {
+  const [photos, setPhotos] = useState<MainPagePhotos[] | []>([]);
+  const div_ref = useRef<HTMLElement>(null);
+  const [imageWidth, setImageWidth] = useState<number>(0);
+
+  const setRefElement = (el: HTMLElement) => {
+    if (!el) return;
+    div_ref.current = el;
+    console.log(div_ref.current?.offsetWidth);
+    setImageWidth(div_ref.current.offsetWidth);
+  };
+
+  window.addEventListener('resize', () => {
+    if (div_ref.current) {
+      setImageWidth(div_ref.current.offsetWidth)
+    }
+  })
+
+  useEffect(() => {
+    axios
+      .get<MainPagePhotos[]>('/photos/photos/?main_page=True')
+      .then(response => {
+        setPhotos(response.data);
+      });
+  }, [])
+
   return (
     <>
-        <section className='container'>
-            XD
+        <section className='container main_page--photos my-3'>
+            <div className='row main_page--photos__container'>
+              {photos.map((photo, index) => {
+                return (
+                  <div ref={ref => { index === photos.length - 1 && setRefElement(ref)  }} key={index} style={{'height':imageWidth}} className='col-4 d-flex justify-content-center align-items-center main_page--photos__container--photo'>
+                    <img src={photo.image} />
+                  </div>
+                );
+              })}
+            </div>
         </section>
     </>
   );
--- src/pages/partials/header.tsx
+++ src/pages/partials/header.tsx
@@ -10,7 +10,6 @@ interface PhotoCategories {
 
 const Header = () => {
     const [categories, setCategories] = useState<PhotoCategories[] | []>([]);
-    // const [error, setError]: [string, (error: string) => void] = useState("");
 
     useEffect(() => {
         axios
@@ -30,19 +29,23 @@ const Header = () => {
                 Emilia Lorentsen Fotografia
                 <a href='https://www.instagram.com/lorentsen.emilia/' target='_blank'><i className="icon-instagram"/></a>
             </h1>
-            <div className="row navigation-panel">
-                <p className="col-3"><a href='/'>Home</a></p>
-                <ul className="col-3">
-                    <p>Categories</p>
-                    <li>
-                        <ul><a href='/categories'>All</a></ul>
-                        {categories.map((category) => {
-                            return <ul key={category.id}><a href={'/categories/' + category.name}>{category.name}</a></ul>
-                        })}
-                    </li>
-                </ul>
-                <p className="col-3"><a href='/sessions'>Sessions</a></p>
-                <p className="col-3"><a href='/about_me'>About me</a></p>
+            <div className="navigation-panel">
+                <div className="container">
+                    <div className="row justify-content-between">
+                        <p className="col-3"><a href='/'>Home</a></p>
+                        <ul className="col-3">
+                            <p>Categories</p>
+                            <li>
+                                <ul><a href='/categories'>All</a></ul>
+                                {categories.map((category) => {
+                                    return <ul key={category.id}><a href={'/categories/' + category.name}>{category.name}</a></ul>
+                                })}
+                            </li>
+                        </ul>
+                        <p className="col-3"><a href='/sessions'>Sessions</a></p>
+                        <p className="col-3"><a href='/about_me'>About me</a></p>
+                    </div>
+                </div>
             </div>
         </header>
     )
