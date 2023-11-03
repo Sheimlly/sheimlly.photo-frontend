a3965f6cdba00c4490fe412a17185ddbb444e052
Emilia <sheimlly.dev@gmail.com>
2023-11-03 21:04:37 +0100

    added about me page

---
+++ src/pages/about_me.tsx
@@ -0,0 +1,16 @@
+const AboutMe = () => {
+    return (
+        <>
+            <section className="about_me--header site_header container my-5">
+                <h2>About me</h2>
+            </section>
+            <section className="container about_me--contact my-5">
+                <h3>Contact info</h3>
+                <p>Email: <span>lorentsenemilia@gmail.com</span></p>
+                <p>Instagram:  <a href='https://www.instagram.com/lorentsen.emilia/' target='_blank'>lorentsen.emilia</a></p>
+            </section>
+        </>
+    )
+}
+
+export default AboutMe;
