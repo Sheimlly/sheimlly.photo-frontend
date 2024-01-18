91672cd4672c028b888ed5a2e555128795a4b409
Sheimlly <sheimlly.dev@gmail.com>
2024-01-18 15:06:39 +0100

    removed unnecesary code from header

--- src/pages/_partials/header.tsx
+++ src/pages/_partials/header.tsx
@@ -50,8 +50,6 @@ const Header = () => {
                 </svg>
             </div>
 
-            {/* <h1 className='title-panel--title order-1 order-md-2'>Emilia Lorentsen</h1> */}
-
             <div id='header__container' ref={navigation} className='header__container'>
                 <svg onClick={hideNavigation} className='header__container--button d-md-none' version="1.1" viewBox="0 0 512 512" width="512px">
                     <path fill="#FFFFFF" d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/>
